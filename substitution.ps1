db6d90393cf9674f23d8f8966c6ae0dbeacd5078
Blair Learn <blairlearn@users.noreply.github.com>
2017-09-18 11:40:18 -0400

    First cut at string substitution

--- substitution.ps1
+++ substitution.ps1
@@ -1,1 +1,35 @@
 #Text substitution
+Param(
+	[Parameter(mandatory=$true, ValueFromPipeline=$false)]
+	[string]$InputFile,
+
+#	[Parameter(mandatory=$true, ValueFromPipeline=$false)]
+#	[string]$OutputFile,
+
+	[Parameter(mandatory=$true, ValueFromPipeline=$false)]
+	[string]$SubstituteList
+)
+
+[xml]$substitutions = Get-Content $SubstituteList
+[string]$data = Get-Content $InputFile
+
+foreach($substitute in $substitutions.substitutions.substitute) {
+
+	$find = $substitute.find
+	$replacement = $substitute.replacement
+	if($replacement.GetType() -eq [System.Xml.XmlElement] ) {
+		if($replacement.FirstChild -ne $null -and $replacement.FirstChild.GetType() -eq [System.Xml.XmlCDataSection]) {
+			$replacement = $replacement.FirstChild.Value
+		} else {
+			Write-Error "Unknown replacement node type."
+			exit
+		}
+	}
+
+	$data = $data.Replace($find, $replacement)
+}
+
+Write-Output $data
+
+
+
